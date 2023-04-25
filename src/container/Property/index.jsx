9c4ae64cc87fb46609bc3e6aaa15eef32c7f9217
cheng.mengling <73818993+Xmengling@users.noreply.github.com>
2023-04-25 10:01:12 +0800

    Initial commit

---
+++ src/container/Property/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './style.module.less';
+
+const property = () => {
+
+  return <div className={s.property}>
+    资产管家
+  </div>
+}
+
+export default property
